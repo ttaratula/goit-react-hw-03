f571f1004052ba88485312ac17508ce3c1019a4e
Tetiana Taratula <ttaratula@gmail.com>
2025-05-04 17:17:00 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import ContactList from "./components/ContactList";
 import css from './App.module.css';
 
 export default function App() {
+  const LOCAL_STORAGE_KEY = "saved-contacts";
+
   const initialContacts = [
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
@@ -13,17 +15,16 @@ export default function App() {
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ];
 
-
+ 
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem("saved-contacts")) ?? initialContacts;
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return saved ? JSON.parse(saved) : initialContacts;
   });
-  
-
 
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
 
   const addContact = (name, number) => {
@@ -31,16 +32,16 @@ export default function App() {
     setContacts((prev) => [...prev, newContact]);
   };
 
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );  
-
   const deleteContact = (id) => {
     setContacts((prev) => prev.filter((contact) => contact.id !== id));
   };
 
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div className={css.container} >
+    <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm onAdd={addContact} />
       <SearchBox filter={filter} onFilterChange={setFilter} />
